862400addb6e1bf287d08c1d07c010f31ff0348a
emcnab <eliot.code@proton.me>
2023-02-28 21:04:34 +0100

    refactor(headers): removed some redundant headers in include/

--- src/comms/byte_send.c
+++ src/comms/byte_send.c
@@ -1,21 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client_receive.h                                   :+:      :+:    :+:   */
+/*   byte_send.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By:  <>                                        +#+  +:+       +#+        */
+/*   By: emcnab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/28 20:51:08 by                   #+#    #+#             */
-/*   Updated: 2023/02/28 20:51:08 by                  ###   ########.fr       */
+/*   Created: 2023/02/28 20:20:20 by emcnab            #+#    #+#             */
+/*   Updated: 2023/02/28 21:02:57 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLIENT_RECEIVE_H
-# define CLIENT_RECEIVE_H
-
-# include <unistd.h>
-# include "s_server.h"
-
-void	client_receive(pid_t pid, t_s_server *client);
-
-#endif
+void	
